f25738c860b980d68273ee914e5e34391bf7046f
Jefferson <jefferson.brandao@g.globo>
2023-07-27 22:10:52 -0300

    refactor

---
+++ src/pages/about.tsx
@@ -0,0 +1,12 @@
+import { BlitzPage } from "@blitzjs/next"
+import React from "react"
+
+const About: BlitzPage = () => {
+  return (
+    <div>
+      <h1>about</h1>
+    </div>
+  )
+}
+
+export default About
